5dec049ed0d1cac2ae90c814c0ff8230755945e3
Shivani Dani <shivanirdani@gmail.com>
2023-01-20 14:46:21 +0530

    day 3

---
+++ 3-Random-Hex-Color-generator/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const color = document.getElementById("color");
+    const btn = document.getElementById("gen");
+
+    const generateHexColor = () => {
+        const randomHexColor = Math.random().toString(16).substring(2,8);
+        this.document.body.style.backgroundColor = "#"+ randomHexColor;
+        color.innerHTML = "#"+ randomHexColor;
+    }
+
+    btn.addEventListener("click", generateHexColor);
+})
