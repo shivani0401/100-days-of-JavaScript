8c6f40499e2bb6ee2b9c3b1e51182bd4bfc18f94
Shivani Dani <shivanirdani@gmail.com>
2023-12-15 19:23:06 +0530

    started day 0

---
+++ 0-All-About-JS/basicJS.js
@@ -0,0 +1,49 @@
+//Datatypes in javascript:
+//Number, String, Boolean, Null, Undefined, Object, Array.
+
+//Difference between var, let, const
+/*Var: Variables declared with var are function-scoped. 
+This means that they are only accessible within the function they are declared in.
+
+Let: Variables declared with let are block-scoped. 
+This means that they are only accessible within the block they are declared in.
+
+Const: Variables declared with const are also block-scoped. 
+However, the value of a const variable cannot be changed after it is defined. 
+It is essentially a constant value.
+
+Var and let can be redeclared, but const cannot. 
+This is a key difference that allows for better control over the variable environment.
+
+Global Var: When you declare a variable with var outside of any function, it becomes a global variable. 
+This can potentially lead to conflicts and bugs.
+*/
+
+//Assignment operator:
+var x = 10;
+//where, var x is a variable that stores the value 10. this storing is done using assignment operator.
+console.log(x); //Outputs: 10
+
+/*Arithmetic Operators:
++, -, /, *, %
+*/
+
+/*Increment & Decrement 
+++, --, +=, -=, *=, /=
+*/
+
+/* Escape Character 
+"\" backslash is known as escape character.
+*/
+var myStr = 'I am a "double quoted" string inside " double quotes" ';
+//Or we can simply use 'single quotes' or `backtics` for avoiding conflicts.
+
+/*Concatenation: + */
+
+/*Length of string is calcylated using .length */
+var count = myStr.length;
+console.log(count);
+
+/*Bracket Notation: [n]-> refers to the nth character of the string also to the nth index of the array */
+
+/*Strings immutability:  */
