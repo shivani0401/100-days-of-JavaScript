f0ec82e353976bff19cc9d789f49a9dec42b20f1
Shivani Dani <shivanirdani@gmail.com>
2023-12-18 17:54:06 +0530

    added basic funtion and type conversion

--- 0-All-About-JS/basicJS.js
+++ 0-All-About-JS/basicJS.js
@@ -58,23 +58,69 @@ there are 2 types of arrays in JS.
 1-d and multi-d */
 
 /*accessing arrays using index*/
-var arr=[5,6,7];
+var arr = [5, 6, 7];
 console.log(arr[0]); // Outputs: 5
 /*same method can be used in accessing element in multi dimentional arrays. */
-var myArray=[[1,2,3],[4,5,6],[7,8,9]];
-var myData=myArray[2][1];
+var myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+var myData = myArray[2][1];
 console.log(myData);
 /*Methods available in Array object:
 push(): pushing data at the end of the array*/
-var myArray=[1,2,3];
+var myArray = [1, 2, 3];
 myArray.push(4);
-console.log(myArray)
+console.log(myArray);
 /*pop(): removes the last element of the array and returns it.*/
 myArray.pop();
-console.log(myArray)
+console.log(myArray);
 /*shift(): removes the first element of the array and returns it.*/
 myArray.shift();
-console.log(myArray)
+console.log(myArray);
 /*unshift(): adds the data at the start of the array.*/
 myArray.unshift(5);
-console.log(myArray)
+console.log(myArray);
+
+/**Function: reusable block of code.
+ * syntax: function functionName(){
+ *      ... declaration and defination of function
+ * }
+ * functionName(); calling function
+ */
+function sayHello() {
+  console.log("Hello World!");
+}
+sayHello();
+/*Scope of the variable: 
+1- local scope (only accessible within that function).
+2- global scope (accessible throughout the program).*/
+var x = 5; //global scope
+function fun() {
+  var y = 6; //local scope
+  console.log(x + y);
+}
+fun(); //Output: 11
+/*If we try to access 'y' from outside of this function then it will give an error because 'y' is not defined there.
+
+/*Type Conversion:
+implicit type conversion happens when we assign a value to a variable with different datatype than the actual value.
+implicit type conversion happens when we assign a value to a variable with different datatype than what is being assigned.
+implicit type conversion happens when we assign a value to a variable with different datatype than what is being assigned.
+implicit type conversion happens when we assign a value to a variable with a different type than the original value.
+1- string to number: parseInt(), parseFloat()
+2- number to string: String()
+3- boolean to string or number: String(), Number()
+4- null or undefined to string: String()
+5- any other type to string: String()*/
+var num = "10";
+console.log(parseInt(num)); //output: 10
+var strNum = "10";
+console.log(parseFloat(strNum)); //output: 10
+var bool = true;
+if (bool) {
+  console.log(String(bool)); //"True"
+} else {
+  console.log(Number(bool)); //0
+}
